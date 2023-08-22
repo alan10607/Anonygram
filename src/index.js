9ad2f929962983cdd005206b824f90fdfc8a69b0
alan10607 <hiden1414@gmail.com>
2023-08-22 15:34:34 +0800

    mv i18n

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { HashRouter as Router } from "react-router-dom";
 import store from "redux/store";
-import i18n from 'util/i18n';//do not remove this import if need i18n
+import i18n from 'config/i18n';//do not remove this import if need i18n
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
